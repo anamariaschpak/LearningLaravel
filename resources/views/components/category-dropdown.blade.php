cb09a3b022ad5951331ed576804e10af4ca252ff
Ana-Maria Schpak <ana.schpak@neovision.dev>
2022-01-18 12:09:57 +0200

    finished Laravel 8 From Scratch:  Section 7 - Filtering

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -1,7 +1,6 @@
 <x-dropdown>
     <x-slot name="trigger">
         <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
-            {{-- @dd($currentCategory) --}}
             {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
             <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
         </button>
@@ -11,7 +10,7 @@
 
     @foreach ($categories as $category)
 
-        <x-dropdown-item href="/?category={{ $category->slug }}"
+        <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
             :active="request()->is('categories/' . $category->slug)">{{ ucwords($category->name) }}
         </x-dropdown-item>
     @endforeach
